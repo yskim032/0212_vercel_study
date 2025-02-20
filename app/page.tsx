13a6f912c01229b172e04325365f58fd99a7c97d
mscyskim032@gmail.com <mscyskim032@gmail.com>
2025-02-20 22:59:40 +0900

    2nd commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h4 className="title">YS Tech</h4>
+      <p className="title">
+        YS Tech is a technology company that provides technology solutions to businesses.
+      </p>
     </div>
   )
 }
