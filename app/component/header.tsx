833f237995cb9e5ce03c0d53a69b056e1bafadec
mscyskim032@gmail.com <mscyskim032@gmail.com>
2025-02-24 23:19:51 +0900

    0224_9

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -6,7 +6,6 @@ export default function Header() {
           <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                 <Image 
                     src="/ys_logo.png"
                     alt="YS Tech Logo" 
@@ -14,8 +13,6 @@ export default function Header() {
                     height={150}
                     className="p-2 rounded-full"
                 />
-
-
                 <span className="ml-3 text-3xl font-bold">YS Tech</span>
               </a>
               <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -25,12 +22,12 @@ export default function Header() {
                 <a className="mr-5 hover:text-gray-900">WEB</a>
               </nav>
               <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Portfolio
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" stroklinejoin-="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </button>
             </div>
-    </header>
+          </header>
     </div>
   )
 }
