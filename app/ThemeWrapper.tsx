79b7b84b15c9c9d65f6d83a1e51ddf5e57aa83f0
mscyskim032@gmail.com <mscyskim032@gmail.com>
2025-02-25 21:35:19 +0900

    0225_3

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
 export function ThemeWrapper({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
